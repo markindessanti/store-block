3ea88649e944c034f731052b876b70d122f3efc7
Markin Dessanti <markindessanti@gmail.com>
2020-08-07 14:49:55 -0300

    5 - Modificando o bloco countdown para ter estilos configuráveis - Contador re-estilizado

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
 
 interface CountdownProps {
   targetDate: string
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+const DEFAULT_TARGET_DATE = (new Date('2020-08-08')).toISOString()
+const CSS_HANDLES = ['countdown']
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  tick(targetDate, setTime)
+
   return (
-    <div>
-      <h1>{targetDate}</h1>
+    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      { `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }
     </div>
   )
 }
